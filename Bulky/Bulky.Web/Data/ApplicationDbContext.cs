728ad54797cd92a9466743a0c8d39dade49aa8b1
Danilo Silva <danilo.silva@msn.com>
2023-10-15 00:44:31 -0300

    Configurando ApplicationDbContext

---
+++ Bulky/Bulky.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
