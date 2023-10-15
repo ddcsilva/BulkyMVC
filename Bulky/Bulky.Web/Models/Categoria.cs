8ab22f7e0a23aaf75ff4a373b375b2374e930aaa
Danilo Silva <danilo.silva@msn.com>
2023-10-15 00:36:07 -0300

    Data Annotations

--- Bulky/Bulky.Web/Models/Categoria.cs
+++ Bulky/Bulky.Web/Models/Categoria.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Bulky.Web.Models
 {
     public class Categoria
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Nome { get; set; }
         public int OrdemExibicao { get; set; }
     }
