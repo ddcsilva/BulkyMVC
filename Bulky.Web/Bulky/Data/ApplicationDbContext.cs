0bbf224aa3bbe06af80d5327336689ce35a98729
danilo.silva <danilo.silva@msn.com>
2023-12-08 20:52:40 -0300

    Configurando o ApplicationDbContext

---
+++ Bulky.Web/Bulky/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.Data;
+
+/// <summary>
+/// Classe que representa o contexto do banco de dados
+/// </summary>
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+}
--- Bulky.Web/Bulky/Program.cs
+++ Bulky.Web/Bulky/Program.cs
@@ -1,7 +1,12 @@
+using Bulky.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Adiciona serviços ao contêiner.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
