59f7ec13cec2800268062e608fc5a4be43224235
danilo.silva <danilo.silva@msn.com>
2023-12-08 23:46:58 -0300

    Obtendo Todas as Categorias

--- Bulky.Web/Bulky/Controllers/CategoriaController.cs
+++ Bulky.Web/Bulky/Controllers/CategoriaController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bulky.Controllers
 {
     public class CategoriaController : Controller
     {
+        private readonly ApplicationDbContext context;
+
+        public CategoriaController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
+            var categorias = context.Categorias.ToList();
             return View();
         }
     }
