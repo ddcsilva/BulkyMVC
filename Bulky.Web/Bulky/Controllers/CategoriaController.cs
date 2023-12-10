a82ecc8c636c8e993d668210f056345127d730ed
danilo.silva <danilo.silva@msn.com>
2023-12-10 18:19:15 -0300

    Criando Categorias

--- Bulky.Web/Bulky/Controllers/CategoriaController.cs
+++ Bulky.Web/Bulky/Controllers/CategoriaController.cs
@@ -1,8 +1,12 @@
 ﻿using Bulky.Data;
+using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bulky.Controllers
 {
+    /// <summary>
+    /// Controller responsável por gerenciar as categorias.
+    /// </summary>
     public class CategoriaController : Controller
     {
         private readonly ApplicationDbContext context;
@@ -12,15 +16,39 @@ namespace Bulky.Controllers
             this.context = context;
         }
 
+        /// <summary>
+        /// Action responsável por listar as categorias.
+        /// </summary>
+        /// <returns> View com a lista de categorias. </returns>
+        [HttpGet]
         public IActionResult Index()
         {
+            // Busca todas as categorias do banco de dados.
             var categorias = context.Categorias.ToList();
+
             return View(categorias);
         }
 
+        /// <summary>
+        /// Action responsável por exibir o formulário de adição de categoria.
+        /// </summary>
+        /// <returns> View com o formulário de adição de categoria. </returns>
+        [HttpGet]
         public IActionResult Adicionar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Adicionar(Categoria categoria)
+        {
+            // Adiciona o objeto categoria ao contexto do banco de dados
+            context.Add(categoria);
+            // Salva as alterações no banco de dados
+            context.SaveChanges();
+
+            // Redireciona para a action Index
+            return RedirectToAction(nameof(Index));
         }
     }
 }
