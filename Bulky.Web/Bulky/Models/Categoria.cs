27003c08adcbe07c67ad992b77322e635a0bda6e
danilo.silva <danilo.silva@msn.com>
2023-12-10 19:57:55 -0300

    Validação Client-Side

--- Bulky.Web/Bulky/Models/Categoria.cs
+++ Bulky.Web/Bulky/Models/Categoria.cs
@@ -11,6 +11,7 @@ public class Categoria
     [MaxLength(30, ErrorMessage = "O nome da categoria deve ter no máximo 30 caracteres.")]
     [DisplayName("Categoria")]
     public string Nome { get; set; }
+    [Required(ErrorMessage = "A ordem de exibição é obrigatória.")]
     [Range(1, 100, ErrorMessage = "A ordem de exibição deve ser entre 1 e 100.")]
     [DisplayName("Ordem de Exibição")]
     public int OrdemExibicao { get; set; }
