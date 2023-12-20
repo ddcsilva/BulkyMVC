215cc5175646449565b9d2eda1fd08515e9f688b
danilo.silva <danilo.silva@msn.com>
2023-12-20 18:41:56 -0300

    Implementar o Repositório de Categorias

---
+++ Bulky/Bulky.DataAccess/Repository/CategoriaRepository.cs
@@ -0,0 +1,25 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+
+namespace Bulky.DataAccess.Repository;
+
+public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
+{
+    private readonly BulkyContext _context;
+
+    public CategoriaRepository(BulkyContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public void Salvar()
+    {
+        _context.SaveChanges();
+    }
+
+    public void Atualizar(Categoria entidade)
+    {
+        _context.Update(entidade);
+    }
+}
--- Bulky/Bulky.DataAccess/Repository/IRepository/ICategoriaRepository.cs
+++ Bulky/Bulky.DataAccess/Repository/IRepository/ICategoriaRepository.cs
@@ -2,6 +2,9 @@
 
 namespace Bulky.DataAccess.Repository.IRepository;
 
+/// <summary>
+/// Interface responsável por definir os métodos de acesso ao banco de dados de Categoria.
+/// </summary>
 public interface ICategoriaRepository : IRepository<Categoria>
 {
     void Atualizar(Categoria entidade);
