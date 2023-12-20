2e781907b022f418cb41cac07eb6c477cc84e873
danilo.silva <danilo.silva@msn.com>
2023-12-20 18:37:06 -0300

    Implementar ICategoryRepository

---
+++ Bulky/Bulky.DataAccess/Repository/IRepository/ICategoriaRepository.cs
@@ -0,0 +1,9 @@
+﻿using Bulky.Models;
+
+namespace Bulky.DataAccess.Repository.IRepository;
+
+public interface ICategoriaRepository : IRepository<Categoria>
+{
+    void Atualizar(Categoria entidade);
+    void Salvar();
+}
