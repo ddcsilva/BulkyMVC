186ee6f7849010f0dcc10b9f16cbdb8b666449ef
danilo.silva <danilo.silva@msn.com>
2024-01-01 19:24:28 -0300

    Melhorando Legibilidade

--- Bulky/Bulky.Models/Categoria.cs
+++ Bulky/Bulky.Models/Categoria.cs
@@ -13,7 +13,9 @@ public class Categoria
     [Required(ErrorMessage = "O nome da categoria é obrigatório.")]
     [MaxLength(30, ErrorMessage = "O nome da categoria deve ter no máximo 30 caracteres.")]
     [DisplayName("Categoria")]
+
     public string Nome { get; set; }
+
     [Required(ErrorMessage = "A ordem de exibição é obrigatória.")]
     [Range(1, 100, ErrorMessage = "A ordem de exibição deve ser entre 1 e 100.")]
     [DisplayName("Ordem de Exibição")]
