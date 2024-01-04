49655afe73eb51aa76aaccd38984ff85e18a19bf
danilo.silva <danilo.silva@msn.com>
2024-01-03 23:19:18 -0300

    Melhoria na Documentação

--- Bulky/Bulky.Web/Areas/Cliente/Controllers/HomeController.cs
+++ Bulky/Bulky.Web/Areas/Cliente/Controllers/HomeController.cs
@@ -4,9 +4,13 @@ using System.Diagnostics;
 
 namespace Bulky.Web.Areas.Cliente.Controllers
 {
+    /// <summary>
+    /// Controller responsável por gerenciar a área do cliente.
+    /// </summary>
     [Area("Cliente")]
     public class HomeController : Controller
     {
+        // ILogger é uma interface que permite a gravação de logs.
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -14,16 +18,19 @@ namespace Bulky.Web.Areas.Cliente.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Action responsável por exibir a página inicial.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+        /// <summary>
+        /// Action responsável por exibir a página de erro.
+        /// </summary>
+        /// <returns></returns>
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
